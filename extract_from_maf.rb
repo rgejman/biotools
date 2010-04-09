b1119c0d495bf46dac013fd682cf11efa2984dd7
Ron Gejman <rsg2119@columbia.edu>
2010-04-09 19:01:34 -0400

    modified:   extract_from_maf.rb

--- extract_from_maf.rb
+++ extract_from_maf.rb
@@ -14,8 +14,8 @@ entries = Dir.entries(directory).select {|e| e =~ /\.maf$/}
 for filename in entries
   puts "Extracting #{filename}"
   output_filename = filename.gsub(".maf", "3way.#{SPECIES.join(".")}.maf")
-  File.open(filename, "r") do |input|
-    File.open(output_filename, "w") do |out|
+  File.open(directory + "/" + filename, "r") do |input|
+    File.open(directory + "/" + output_filename, "w") do |out|
       input.each_line do |line|
         if line.chomp == ""
           out.print line
